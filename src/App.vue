7b53d395e90aab8a6adc343388ac4209c892e1cb
Mikael Engström <micke@primat.se>
2022-05-10 10:48:42 +0200

    Dist included

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png"/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Testx/>hej
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Testx from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Testx
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hejsan</h1>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
